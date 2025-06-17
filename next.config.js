f1262b4fd7b406b7212b6b4111d3ba3df6f8a872
Vansh Jain <95092012+vanshjainofficial@users.noreply.github.com>
2025-06-17 10:50:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
-const { Life_Savers } = require('next/font/google');
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production' ? true : false,
-  },
-
-  webpack: (config) => {
-
-    class VeliteWebpackPlugin {
-      static started = false;
-      apply(compiler) {
-        compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-          if (VeliteWebpackPlugin.started) return;
-          VeliteWebpackPlugin.started = true;
-          const dev = compiler.options.mode === 'development';
-          const { build } = await import('velite');
-          await build({ watch: dev, clean: !dev });
-        });
-      }
-    }
-
-
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
+const config = {
+  siteUrl: 'https://vanshjainblog.vercel.app',
+  generateRobotsTxt: true, 
+  sitemapSize: 7000, 
+  changefreq: 'weekly',
+  priority: 0.7,
+  trailingSlash: false,
 };
+
+module.exports = config;
