e6684074dcbf4d8a9806e4f3ed4613d999ec171f
Vansh Jain <95092012+vanshjainofficial@users.noreply.github.com>
2025-06-17 12:18:52 +0530

    Update ViewCounter.js

--- src/components/Blog/ViewCounter.js
+++ src/components/Blog/ViewCounter.js
@@ -1,37 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
-  const [views, setViews] = useState(0);
-
-
-  useEffect(() => {
-    const incrementView = () => {
-      const storedViews = localStorage.getItem(slug);
-      const newViews = storedViews ? parseInt(storedViews, 10) + 1 : 1;
-
-
-      localStorage.setItem(slug, newViews);
-      setViews(newViews);
-    };
-
-    if (!noCount) {
-      incrementView();
-    }
-  }, [slug, noCount]);
-
-
-  useEffect(() => {
-    const storedViews = localStorage.getItem(slug);
-    setViews(storedViews ? parseInt(storedViews, 10) : 0);
-  }, [slug]);
-
-
-  if (showCount) {
-    return <div>{views} views</div>;
-  } else {
-    return null;
-  }
+const ViewCounter = ({ showCount = true }) => {
+  if (!showCount) return null;
+  return <div>1k+ views</div>;
 };
 
 export default ViewCounter;
