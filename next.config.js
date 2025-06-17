55cb1bef8c2502694d1f82eb827c3cdfd12f7a5c
Vansh Jain <95092012+vanshjainofficial@users.noreply.github.com>
2025-06-17 11:08:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,23 +5,4 @@ module.exports = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-
-  webpack: (config) => {
-    class VeliteWebpackPlugin {
-      static started = false;
-
-      apply(compiler) {
-        compiler.hooks.beforeRun.tapPromise('VeliteWebpackPlugin', async () => {
-          if (VeliteWebpackPlugin.started) return;
-          VeliteWebpackPlugin.started = true;
-          const dev = compiler.options.mode === 'development';
-          const { build } = await import('velite');
-          await build({ watch: dev, clean: !dev });
-        });
-      }
-    }
-
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
 };
